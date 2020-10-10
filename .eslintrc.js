6597e389a5f7175edc0ff2e887647f9fe60eea65
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-10-10 10:51:46 +0900

    Installing Parcel

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,14 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    "no-trailing-spaces": 0,
+    "keyword-spacing": 0,
+    "no-unused-vars": 1,
+    "no-multiple-empty-lines": 0,
+    "space-before-function-paren": 0,
+    "eol-last": 0,
+  },
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import App from "./App.js";
+import "./scss/style.scss";
 
 const app = new App();
